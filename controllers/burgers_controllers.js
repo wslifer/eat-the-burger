47c29de20c424b77ebe7aee2d49821def2bc0a14
Wesley Slifer <wesleyslifer@ymail.com>
2020-06-14 17:17:21 -0500

    added code for controller file

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -1,2 +1,31 @@
 const express = require("express");
 const burger = require("../models/burger");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  burger.selectAll((data) => {
+    const hbsObject = {
+      burgers: data,
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
+
+router.post("/", (req, res) => {
+  burger.insertOne(req.body.burger_name, () => {
+    res.redirect("/");
+  });
+});
+
+router.put("/:id", (req, res) => {
+  const id = req.params.id;
+
+  console.log("id", id);
+
+  burger.updateOne(id, () => {
+    res.redirect("/");
+  });
+});
+
+module.exports = router;
