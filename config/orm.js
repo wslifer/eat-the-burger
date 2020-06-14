a868f313df8a008c71182bf35a772742df443ea0
Wesley Slifer <wesleyslifer@ymail.com>
2020-06-14 16:51:28 -0500

    adding config files and installing packages

---
+++ config/orm.js
@@ -0,0 +1,31 @@
+const connection = require("./connection");
+
+const orm = {
+  selectAll: function (cb) {
+    const querystring = `SELECT * FROM burgers_db`;
+
+    connection.query(querystring, (err, res) => {
+      if (err) throw err;
+      cb(res);
+    });
+  },
+
+  insertOne: function (burgers, cb) {
+    const querystring = `INSERT INTO burgers (burger_name) VALUES (?)`;
+    connection.query(querystring, (err, res) => {
+      if (err) throw err;
+      cb(res);
+    });
+  },
+
+  updateOne: function (id, cb) {
+    const querystring = `UPDATE burgers SET devoured = true WHERE id = ?`;
+
+    connection.query(querystring, (err, res) => {
+      if (err) throw err;
+      cb(res);
+    });
+  },
+};
+
+module.exports = orm;
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers(burger_name) VALUES ('Taco Burger');
+INSERT INTO burgers(burger_name) VALUES ('Bacon Cheese Burger');
+INSERT INTO burgers(burger_name) VALUES ('Turkey Burger');
+INSERT INTO burgers(burger_name) VALUES ('Tofu Burger');
