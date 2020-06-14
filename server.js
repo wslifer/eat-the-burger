4ff3c83e887972b7c6d04f1a6ecedeed9f5d2835
Wesley Slifer <wesleyslifer@ymail.com>
2020-06-14 17:38:17 -0500

    wrote code in server.js in order to use express app and use handlebars view files

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
 const express = require("express");
+const exphbs = require("express-handlebars");
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view_engine", "handlebars");
+
+const routes = require("./controllers/burgers_controllers");
+
+app.use(routes);
+
+app.listen(PORT, () => {
+  console.log(`App listening at localhost: ${PORT}`);
+});
