4199ad53009348180b320c5e8ceacddd6fa107a4
Wesley Slifer <wesleyslifer@ymail.com>
2020-06-14 17:01:07 -0500

    completed burger.js file

--- models/burger.js
+++ models/burger.js
@@ -1,1 +1,22 @@
 const orm = require("../config/orm");
+const { insertOne } = require("../config/orm");
+
+const burger = {
+  selectAll: function (cb) {
+    orm.selectAll((res) => {
+      cb(res);
+    });
+  },
+
+  insertOne: function (cb) {
+    orm.insertOne((res) => {
+      cb(res);
+    });
+  },
+
+  updateOne: function (cb) {
+    orm.updateOne((res) => {
+      cb(res);
+    });
+  },
+};
