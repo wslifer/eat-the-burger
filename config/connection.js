5ab6c9c2390f5b1b96cc23c36e080a272867bab4
Wesley Slifer <wesleyslifer@ymail.com>
2020-07-31 10:15:46 -0500

    fixing jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,19 @@
-const mysql = require("mysql");
+var mysql = require("mysql");
 
-const connection;
-
+var connection;
 if (process.env.JAWSDB_URL) {
   // Connection for running on Heroku
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   // Connection for running on localhost.
   connection = mysql.createConnection({
-      host: 'localhost',
-      port: 3306,
-      user: 'root',
-      password: 'password',
-      database: 'burgers_db'
-  })
-};
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "burgers_db",
+  });
+}
 
 // Connection function
 connection.connect(function (err) {
@@ -24,5 +23,6 @@ connection.connect(function (err) {
   }
   console.log("connected as id " + connection.threadId);
 });
+
 // Exports function to be used in other files.
 module.exports = connection;
